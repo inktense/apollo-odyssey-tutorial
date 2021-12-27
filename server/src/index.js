34a562754971cc536d68603e7b1bad074d822703
Bianca Popescu <bianca.popescu@biancas-mbp.home>
2021-12-27 15:04:21 +0200

    Added mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,40 @@
-//TODO
+import { ApolloServer } from "apollo-server";
+import typeDefs from "./schema";
+
+// With mocks enabled, Apollo Server always returns exactly two entries for every list field.
+// To get more entries at a time, let's say 6, we'll add a Query.tracksForHome to our mocks object and return an 
+// Array of that given length like so: [...new Array(6)].
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(6)],
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  //To enable basic mocked data, we could provide mocks:true to the ApolloServer constructor.
+  mocks,
+});
+
+server.listen().then(() => {
+  console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 4000
+      📭  Query at https://studio.apollographql.com/dev
+    `);
+});
